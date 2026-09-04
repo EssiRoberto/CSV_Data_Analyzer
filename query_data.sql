0762c429d2e841b414c670bb3cac384ec4f2c0ae
EssiRoberto <essiroberto@gmail.com>
2026-09-04 21:58:06 +0100

    Fix SQL schema and query

--- query_data.sql
+++ query_data.sql
@@ -1,8 +1,2 @@
-CREATE TABLE sales (
-    id INTEGER PRIMARY KEY,
-    item TEXT NOT NULL,
-    amount REAL NOT NULL
-);
-
 SELECT item, amount FROM sales WHERE amount > 20.0 ORDER BY amount DESC;
 
